2e474f99e0dc7ee0a263508b9da55a81914c40e7
Paulina <aniskiewicz.paulina@gmail.com>
2019-12-11 12:10:25 +0100

    Links to images and contacts added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "A pair of warm, fuzzy socks"
+    description: "A pair of warm, fuzzy socks",
+    image: "/pictures/green-socks.png",
+    altText: "Green socks",
+    linkContactUs: "https://goodday4u.com/"
   }
 });
