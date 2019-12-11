7f68132d0ae3805ea6f4bfc3982df59a2a673560
Paulina <aniskiewicz.paulina@gmail.com>
2019-12-11 12:40:14 +0100

    v-if/v-else condition added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var app = new Vue({
     description: "A pair of warm, fuzzy socks",
     image: "/pictures/green-socks.png",
     altText: "Green socks",
-    linkContactUs: "https://goodday4u.com/"
+    linkContactUs: "https://goodday4u.com/",
+    inStock: false
   }
 });
